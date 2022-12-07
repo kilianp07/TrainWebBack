cf42c6ea3d1ea60aa5a1b425bf083cd386743737
alexjnj1301 <alexis.jeanjean@ynov.com>
2022-12-07 16:09:53 +0100

    initialisation connexion BDD

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    "TrainWeb", 
+    "root", 
+    "root", 
+    {dialect: "mysql", host: "127.0.0.1"});
+
+
+sequelize.authenticate();   
+console.log('Connecté à la base de données MySQL!');
+sequelize.query("SELECT * FROM `Users`", { type: sequelize.QueryTypes.SELECT}).then((results) => {       
+    console.log(results);     
+});
+
