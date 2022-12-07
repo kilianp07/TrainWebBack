eaf5fa4f2c88b21c9d96232c5479065a35c7d616
alexjnj1301 <alexis.jeanjean@ynov.com>
2022-12-07 16:29:40 +0100

    create User

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,15 @@ const sequelize = new Sequelize(
 
 sequelize.authenticate();   
 console.log('Connecté à la base de données MySQL!');
-sequelize.query("SELECT * FROM `Users`", { type: sequelize.QueryTypes.SELECT}).then((results) => {       
-    console.log(results);     
-});
+createUser();
+getUsers();
 
+function getUsers() {
+    return sequelize.query("SELECT * FROM `Users`", { type: sequelize.QueryTypes.SELECT}).then((results) => {       
+        console.log(results);     
+    });
+}
+
+function createUser() {
+    return sequelize.query("INSERT INTO `Users` (`id`, `email`, `password`, `username`) VALUES (NULL, 'kylian.peyron@ynov.com', 'Kylian_pyrn0912', 'Kylian Peyron');");
+}
