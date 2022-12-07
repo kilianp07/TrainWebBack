c84796c6f32aa84b3f045f13f72c236294bd9429
Jarod Laquet <jarod.laquet@ynov.com>
2022-12-07 15:17:31 +0100

    Create_Table_Answer

---
+++ migrations/20221207141700-create-answer.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Answers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      value: {
+        type: Sequelize.STRING
+      },
+      suspended: {
+        allowNull : true,
+        type: Sequelize.BOOLEAN
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Answers');
+  }
+};
