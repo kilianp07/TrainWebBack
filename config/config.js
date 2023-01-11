466771ed178e347cf82aa841f38cb0846dbbb940
kilianp07 <kilianp07@icloud.com>
2023-01-11 11:09:52 +0100

    Sequelize stqrt configuration

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
-{
+require('dotenv').config();
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "password",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
     "database": "database_development",
     "host": "127.0.0.1",
     "dialect": "mysql"
