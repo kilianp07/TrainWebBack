f01932f1bd6a616ad97850d16b5c83fd07f1a810
JarodLaquetDev <109896606+JarodLaquetDev@users.noreply.github.com>
2023-01-18 15:19:41 +0100

    Crud answer (#17)
    * Answer getbyid/getall
    
    * Answer update

--- routes/answer.js
+++ routes/answer.js
@@ -27,6 +27,23 @@ Answer.incomingCorrectlyFilled = (incomingAnswer)=>{
     incomingAnswer.suspended != null;
 }
 
+router.get('/getbyid/:id', async(req, res, next) => {
+  const id = req.params.id;
+  const answer = await Answer.findOne({where: {id: id}});
+  if(answer == null) {
+    res.status(StatusCodes.NOT_FOUND).json({message: "Answer not found"})
+    return};
+  res.status(StatusCodes.OK).json({answer});
+});
+
+router.get('/getall', async(req, res, next) => {
+  const answers = await Answer.findAll({where : {isDeleted: false}});
+  if(answers.length == 0) {
+    res.status(StatusCodes.NOT_FOUND).json({message: "No answers found"})
+    return};
+  res.status(StatusCodes.OK).json({answers});
+});
+
 router.post('/create', async(req,res,next) => {
     if (Answer.incomingCorrectlyFilled(req.body.Answer) == false) {
        res.status(StatusCodes.BAD_REQUEST).json({message: "Missing parameters"})
@@ -60,5 +77,14 @@ router.delete('/harddelete/:id', async(req, res, next) => {
     res.status(StatusCodes.OK).json({message: "Answer deleted from database"});
 });
 
+router.put('/update/:id', async(req, res, next) => {
+  const id = req.params.id;
+  const answer = await Answer.findOne({where: {id: id}});
+  if(answer == null) {
+    res.status(StatusCodes.NOT_FOUND).json({message: "Answer not found"})
+    return};
+  await Answer.update(req.body.Answer, {where: {id: id}});
+  res.status(StatusCodes.OK).json({message: "Answer updated"});
+});
 
 module.exports = router;
