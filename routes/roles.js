4cde5868cdeca5d7a08d8f8176d3c9cf58ae141f
Jarod Laquet <jarod.laquet@ynov.com>
2023-01-18 14:40:29 +0100

    Role delete/harddelete

--- routes/roles.js
+++ routes/roles.js
@@ -32,11 +32,35 @@ router.get('/getbyid/:id', async(req, res, next) => {
 });
 
 router.get('/getall', async(req, res, next) => {
-    const roles = await Role.findAll({where : isDeleted = false});
+    const roles = await Role.findAll({where : {isDeleted: false}});
     if(roles.length == 0) {
       res.status(StatusCodes.NOT_FOUND).json({message: "No roles found"})
       return};
     res.status(StatusCodes.OK).json({roles});
 });
+
+router.delete('/delete/:id', async(req, res, next) => {
+    const id = req.params.id;
+    const role = await Role.findOne({where: {id: id}});
+    if(role == null) {
+      res.status(StatusCodes.NOT_FOUND).json({message: "Role not found"})
+      return};
+    if(role.isDeleted == true) {
+    res.status(StatusCodes.OK).json({message: "Role already deleted"});
+      return
+    }
+    await Role.update({isDeleted : true}, {where: {id: id}});
+    res.status(StatusCodes.OK).json({message: "Role deleted"});
+  });
+  
+  router.delete('/harddelete/:id', async(req, res, next) => {
+    const id = req.params.id;
+    const role = await Role.findOne({where: {id: id}});
+    if(role == null) {
+      res.status(StatusCodes.NOT_FOUND).json({message: "Role not found"})
+      return};
+    await Role.destroy({where: {id: id}});
+    res.status(StatusCodes.OK).json({message: "Role deleted from database"});
+  });
 
 module.exports = router;
