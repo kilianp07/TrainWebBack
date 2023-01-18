7ebee999d88a60420213fa491d7aa2d43b21b9f6
kilianp07 <57315153+kilianp07@users.noreply.github.com>
2023-01-18 12:01:01 +0100

    Refacto: Specific methods placed into sequelize models (#9) (#13)
    * Specific methods placed into sequelize models
    
    * Specific methods placed into sequelize models
    
    * modified:   routes/users.js

--- models/token.js
+++ models/token.js
@@ -1,5 +1,6 @@
 'use strict';
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 const {
   Model
 } = require('sequelize');
@@ -25,6 +26,32 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   Token.verify = async function(token) {
+    var privateKey = fs.readFileSync(process.env.SECRET_KEY);
+    try {
+      const decoded = jwt.decode(token, {complete: true});
+      if (!decoded || !decoded.payload || !decoded.payload.exp) {
+          console.log("Invalid token");
+          return false;
+      }
+      const expirationTime = decoded.payload.exp;
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (currentTime >= expirationTime) {
+          console.log("Token expired");
+          return false;
+      }
+      // Token is valid and not expired
+      jwt.verify(token, privateKey);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  Token.generate = async function(Userid) {
+    var privateKey = fs.readFileSync(process.env.SECRET_KEY);
+    var token = {
+      token: jwt.sign({id: Userid}, privateKey, {expiresIn: process.env.TOKEN_DURABILITY}),
+      expirationDate: Date.now() + Number(process.env.TOKEN_DURABILITY),
     try{
       jwt.verify(token, process.env.SECRET_KEY)
     }catch(err){
