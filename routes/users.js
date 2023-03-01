601ffeecdacf0d42a2a43e0d6c40d724b56e88ec
Nino <nino.didier@live.fr>
2023-03-01 17:30:54 +0100

    fix

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   }
 )
 const User = require('../models/user')(sequelize, Sequelize.DataTypes,Sequelize.Model);
-const Role = require('./answer')(sequelize, Sequelize.DataTypes,Sequelize.Model);
+const Role = require('../models/role')(sequelize, Sequelize.DataTypes,Sequelize.Model);
 const Token = require('../models/token')(sequelize, Sequelize.DataTypes,Sequelize.Model);
 
 
