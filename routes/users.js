030d7ee854f6f6c6d4c7557b8edcbf43901a8452
Nino <77067659+Ninotter@users.noreply.github.com>
2023-04-12 14:18:27 +0200

    Rename migration order + rework idRole (#66)

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   }
 )
 const User = require('../models/user')(sequelize, Sequelize.DataTypes,Sequelize.Model);
+const Role = require('../models/role')(sequelize, Sequelize.DataTypes,Sequelize.Model);
 const Token = require('../models/token')(sequelize, Sequelize.DataTypes,Sequelize.Model);
 
 
@@ -59,13 +60,14 @@ router.post('/student/create', async(req,res) => {
       return
     }
   
+    const studentRole = await Role.findOne({ where: { value: "STUDENT" } })
     // Create user
     var usr = {
       email: incomingUser.email,
       username: incomingUser.username,
       password: await bcrypt.hash(incomingUser.password, salt),
       emailVerified: false,
-      role: "STUDENT"
+      idRole: studentRole.id
     }
     const createdUser = await User.create(usr)
     res.status(StatusCodes.StatusCodes.CREATED).json({createdUser, message: "User created"})
@@ -91,7 +93,11 @@ router.post('/teacher/create', async(req,res) => {
   }
 
     const user = await User.findOne({ where: { id: token.idUser } })
-    if(user.role != "ADMIN" || user.role != "TEACHER") {
+    
+    const teacherRole = await Role.findOne({ where: { value: "TEACHER" } })
+    const adminRole = await Role.findOne({ where: { value: "ADMIN" } })
+
+    if(user.idRole != adminRole.id || user.idRole != teacherRole.id) {
       res.status(StatusCodes.StatusCodes.FORBIDDEN).json({message: "You must be an admin or a teacher to create a teacher"})
       return
     }
@@ -121,7 +127,7 @@ router.post('/teacher/create', async(req,res) => {
       username: incomingUser.username,
       password: await bcrypt.hash(incomingUser.password, salt),
       emailVerified: false,
-      role: "TEACHER"
+      idRole: teacherRole.id
     }
     const createdUser = await User.create(usr)
     res.status(StatusCodes.StatusCodes.CREATED).json({createdUser, message: "User created"})
